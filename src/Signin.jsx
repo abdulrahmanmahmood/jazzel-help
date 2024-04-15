54ae7069941c50aab016d126a6c83b054bb3ac32
abdulrahmanmahmood <abdulrahmanabdultawab189_sd@fcis.bsu.edu.eg>
2024-04-15 22:00:07 +0200

    edit the auth pages logos

--- src/Signin.jsx
+++ src/Signin.jsx
@@ -8,6 +8,10 @@ import background from "./assets/background.png";
 import nextLogo from "./assets/nextLogo.png";
 import baseLogo from "./assets/baseLogo.png";
 
+import logo1 from "./assets/ub 1.png";
+import logo2 from "./assets/agaweed.png";
+import logo3 from "./assets/gazl.png";
+
 const Signin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -154,9 +158,27 @@ const Signin = () => {
           </form>
         </div>
       </div>
-      <div className="w-full h-[20vh] bg-[#CEB99E]">
-        <h1 className="text-white text-[24px] font-[800] mr-5 pt-5 text-right ">
-شركاء النجاح        </h1>
+      <div className="w-full max-md:h-[20vh] h-[30vh] bg-[#CEB99E]">
+        <h1 className="text-white text-4xl pr-7 font-[800] mr-5 pt-5 text-right ">
+          شركاء النجاح{" "}
+        </h1>
+        <div className="max-md:h-[10vh] h-[20vh] flex flex-row-reverse  items-baseline justify-start pr-10">
+          <img
+            src={logo1}
+            className="max-md:w-[60px] max-md:h-[60px] w-[120px] h-[120px] mx-[20px] "
+            alt="Logo1"
+          />
+          <img
+            src={logo2}
+            className="max-md:w-[60px] max-md:h-[60px] w-[120px] h-[120px] mx-[20px] "
+            alt="Logo2"
+          />
+          <img
+            src={logo3}
+            className="max-md:w-[60px] max-md:h-[60px] w-[120px] h-[120px] mx-[20px] "
+            alt="besha"
+          />
+        </div>
       </div>
     </div>
   );
